b1e7fdb1fb355a39876aa263030f26a8bba8c048
Alwisteins <alwi.wahyu.w@gmail.com>
2025-08-25 20:14:27 +0700

    feat(html & css): add mini project 1 & mini project 2

--- 1-html-&-css/projects.js
+++ 1-html-&-css/projects.js
@@ -10,6 +10,23 @@ const projects = [
     link: "/semester-1/1-html-&-css/mini-project-1/index.html",
   },
   {
+    title: "Profile Page",
+    emoji: "🎨",
+    desc: "A personal profile page built with semantic HTML layout, featuring a clean structure with header, navigation, main content, and footer.",
+    tag: "Mini Project 2",
+    lesson: "Layout Structure & Semantic HTML",
+    link: "/semester-1/1-html-&-css/mini-project-2/index.html",
+  },
+  {
+    title: "Contact me Page",
+    emoji: "🎨",
+    desc: "A simple contact page containing a user input form (name, email, and message) along with a structured table displaying education and experience history.",
+    tag: "Mini Project 3",
+    lesson: "Form + Table",
+    link: "/semester-1/1-html-&-css/mini-project-3/index.html",
+  },
+
+  {
     title: "Online Courses",
     emoji: "🎨",
     desc: "Comprehensive weekly assignment demonstrating advanced HTML/CSS techniques and modern layout methods.",
